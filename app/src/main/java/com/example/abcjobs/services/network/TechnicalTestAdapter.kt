cabb6b8cdecb0c25fc9541e13cca14b345062d16
alejo44b <alejandrobustacara44@gmail.com>
2023-11-20 11:45:53 -0500

    fix: AWS network configuration

--- app/src/main/java/com/example/abcjobs/services/network/TechnicalTestAdapter.kt
+++ app/src/main/java/com/example/abcjobs/services/network/TechnicalTestAdapter.kt
@@ -16,8 +16,8 @@ import kotlin.coroutines.suspendCoroutine
 
 class TechnicalTestAdapter constructor(context: Context) {
     companion object {
-        //const val BASE_URL = "http://10.0.2.2:3006"
-        const val BASE_URL = "http://lb-tectest-1541669127.us-east-1.elb.amazonaws.com"
+        const val BASE_URL = "http://10.0.2.2:3006"
+        //const val BASE_URL = "http://lb-tectest-1541669127.us-east-1.elb.amazonaws.com"
         private var instance: TechnicalTestAdapter? = null
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
@@ -35,15 +35,6 @@ class TechnicalTestAdapter constructor(context: Context) {
                 cont.resumeWithException(it)
             }))
     }
-/*
-*     val companyId: Int,
-    val companyName: Int,
-    val date: String,
-    val id: Int,
-    val itSpecialistId: String,
-    val itSpecialistName: String,
-    val projectId: String,
-    val projectName: String*/
     suspend fun getTechnicalTests(token: String): Array<TechnicalTest> = suspendCoroutine { cont ->
         requestQueue.add(getRequestJsonArray("/technical_tests",
             token,
